0d7cd2ffbecb14f7b294ea84bd72d259d2aa91f8
Bhaskar Biswas <bhaskarbiswas.economics@gmail.com>
2022-01-21 10:11:02 +0530

    update share text

--- streamlit-apps/dreamxi_visuals/dream_xi.py
+++ streamlit-apps/dreamxi_visuals/dream_xi.py
@@ -100,11 +100,13 @@ expn = get_expn_val(rule)
 reward_prop = get_reward(share, expn, Atanu,Bhaskar,Deb,Rajani)
 
 reward_dict = reward_prop.set_index('Name')['Profit/Loss'].to_dict()
+share_txt = "Share: " + share
 rule_txt = "Rule: " + rule
 if reward_prop.shape[0] == 0:
     st.markdown("Enter values in the sidebar")
 else:
     st.header(match_name)
+    st.subheader(share_txt)
     st.subheader(rule_txt)
     st.dataframe(reward_prop)
     get_plot_bar_graph(reward_prop)
